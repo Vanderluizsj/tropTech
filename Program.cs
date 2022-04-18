7f67a0556d90e7574675a3d052506399a76f6807
Vanderluizsj <48038130+Vanderluizsj@users.noreply.github.com>
2022-04-18 10:43:44 -0300

    start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tropTech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
