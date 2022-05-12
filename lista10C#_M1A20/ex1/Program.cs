6671e82f26093548a0de877a75464f8f912e301a
Vanderluizsj <48038130+Vanderluizsj@users.noreply.github.com>
2022-05-12 08:02:33 -0300

    atualização

---
+++ lista10C#_M1A20/ex1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Digite o numero de trabalhadores avaliados: ");
+            int qtdTrabalhadores = int.Parse(Console.ReadLine());
+            string[] nome = new string[qtdTrabalhadores];
+            int[] idade = new int[qtdTrabalhadores];
+            int[] tempoTrabalho = new int[qtdTrabalhadores];
+		
+		
+            for(int i =0; i < qtdTrabalhadores; i++){
+                Console.WriteLine("Digite o nome do trabalhadore a ser avaliado: ");
+                nome[i] = Console.ReadLine();
+            
+                Console.WriteLine("Digite o idade dos trabalhador a ser avaliado: ");
+                idade[i] = int.Parse(Console.ReadLine());
+            
+                Console.WriteLine("Digite o tempo de trabalho do trabalhador a ser avaliado: ");
+                tempoTrabalho[i] = int.Parse(Console.ReadLine());
+            }
+    }
+}
