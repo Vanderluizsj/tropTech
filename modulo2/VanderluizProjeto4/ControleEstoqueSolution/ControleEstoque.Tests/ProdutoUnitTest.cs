4dfbaf5e8dc27e6e0ed006ecfa6e8ad4e1c84d68
Vanderluizsj <48038130+Vanderluizsj@users.noreply.github.com>
2022-06-26 23:03:17 -0300

    criação do projeto 4 do modulo 2

---
+++ modulo2/VanderluizProjeto4/ControleEstoqueSolution/ControleEstoque.Tests/ProdutoUnitTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace ControleEstoque.Tests
+{
+    public class ProdutoUnitTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
