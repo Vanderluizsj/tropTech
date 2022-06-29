0e7267cb47824a46243ce37db9b0a018bdefc910
Vanderluizsj <48038130+Vanderluizsj@users.noreply.github.com>
2022-06-29 03:20:07 -0300

    projeto4 do modulo2

--- modulo2/VanderluizProjeto4/ControleEstoqueSolution/ControleEstoque.Tests/EstoqueUnitTest.cs
+++ modulo2/VanderluizProjeto4/ControleEstoqueSolution/ControleEstoque.Tests/EstoqueUnitTest.cs
@@ -1,5 +1,5 @@
 using NUnit.Framework;
-
+using ControleEstoque.Excecoes;
 namespace ControleEstoque.Tests
 {
     public class EstoqueUnitTest
@@ -9,10 +9,112 @@ namespace ControleEstoque.Tests
         {
         }
 
+        /*1 - Quando cadastrar produto no estoque e produto ainda não existir no estoque então deve concluir cadastro com sucesso*/
         [Test]
-        public void Test1()
+        public void CadastroDeProdutoOk()
         {
-            Assert.Pass();
+            //arrange
+            var estoque = new Estoque();
+            var produto = new Produto("carro", "novo");
+            //action
+            var produtoCadastrado = estoque.CadastrarProdutoNoEstoque("carro", "novo");
+            //assert
+            Assert.AreEqual(produto, produtoCadastrado);       
+           
+        }
+
+        /* 2 - Quando cadastrar produto no estoque e produto já existir no estoque então não deve concluir cadastro*/
+        [Test]
+        public void FalhaPoisProdutoJaFoiCadastrado()
+        {
+            //arrange
+            var estoque = new Estoque();
+            //action
+            estoque.CadastrarProdutoNoEstoque("carro", "novo");
+            //assert
+            ProdutoJaCadastradoException excecao = Assert.Throws<ProdutoJaCadastradoException>(()=>estoque.CadastrarProdutoNoEstoque("carro", "novo"));       
+           
+        }
+        /* 3 - Quando registrar a entrada de produtos no estoque e produto não existir no estoque então não deve concluir a ação*/
+        [Test]
+        public void FalhaPoisProdutoNaoExisteEntaoNaoPodeRegistrar()
+        {
+            //arrange
+            var estoque = new Estoque();
+            //action e assert
+            ProdutoNaoEncontradoException excecao = Assert.Throws<ProdutoNaoEncontradoException>(()=>estoque.RegistrarEntradaDeProduto(1, 1));       
+           
+        }
+        /* 4 - Quando registrar a entrada de 5 itens de produtos no estoque e produto existir no estoque então deve concluir a ação*/
+        [Test]
+        public void RegistroBemSucedidoDeItemNoEstoque()
+        {
+            //arrange
+            var estoque = new Estoque();
+            var produto = new Produto("carro", "novo");
+            produto.DefineId();
+            //action
+            var produtoCadastrado = estoque.CadastrarProdutoNoEstoque("carro", "novo");
+            estoque.RegistrarEntradaDeProduto(1, 5);
+            //assert
+            Assert.IsTrue(estoque.HaProdutosEmEstoque());                    
+           
+        }
+        /* 5 - Quando registrar a saída de 5 itens de produtos no estoque e produto não existir no estoque então não deve concluir a ação*/
+        [Test]
+        public void FalhaPoisProdutoNaoExisteEntaoNaoPodeRegistrarSaida()
+        {
+            //arrange
+            var estoque = new Estoque();
+            //action e assert
+            ProdutoNaoEncontradoException excecao = Assert.Throws<ProdutoNaoEncontradoException>(()=>estoque.RegistrarSaidaDeProduto(1, 1));       
+           
+        }
+
+        /* 6 - Quando registrar a saída de 5 itens de produtos no estoque e produto existir no estoque com quantidade maior ou igual a 5 então deve concluir a ação*/
+        [Test]
+        public void RegistroDeSaidaBemSucedidoDeItemDoEstoque()
+        {
+            //arrange
+            var estoque = new Estoque();
+            var produto = new Produto("carro", "novo");
+            produto.DefineId();
+            //action
+            var produtoCadastrado = estoque.CadastrarProdutoNoEstoque("carro", "novo");
+            estoque.RegistrarEntradaDeProduto(1, 7);
+            estoque.RegistrarSaidaDeProduto(1, 5);
+            //assert
+            Assert.IsTrue(estoque.HaProdutosEmEstoque());                    
+           
+        }
+        /* 7 - Quando verificar se há itens no estoque e quantidade de itens for igual a zero então deve informar que não há*/
+
+         [Test]
+        public void VerificacaoDeProdutosNoEstoqueQuandoNaoTemProdutosNoEstoque()
+        {
+            //arrange
+            var estoque = new Estoque();
+            Assert.IsFalse(estoque.HaProdutosEmEstoque());                    
+           
+        }
+
+
+        /* 8 - Quando verificar se há itens no estoque e quantidade de itens for maior que zero então deve informar que há*/
+        
+        [Test]
+        public void VerificacaoDeItemNoEstoqueQuandoQuantidadeMaiorQue0()
+        {
+            //arrange
+            var estoque = new Estoque();
+            var produto = new Produto("carro", "novo");
+            produto.DefineId();
+            //action
+            var produtoCadastrado = estoque.CadastrarProdutoNoEstoque("carro", "novo");
+            estoque.RegistrarEntradaDeProduto(1, 7);
+            
+            //assert
+            Assert.IsTrue(estoque.HaProdutosEmEstoque());                    
+           
         }
     }
 }
--- modulo2/VanderluizProjeto4/ControleEstoqueSolution/ControleEstoque.Tests/ProdutoUnitTest.cs
+++ modulo2/VanderluizProjeto4/ControleEstoqueSolution/ControleEstoque.Tests/ProdutoUnitTest.cs
@@ -1,3 +1,5 @@
+using System;
+using ControleEstoque.Excecoes;
 using NUnit.Framework;
 
 namespace ControleEstoque.Tests
@@ -9,10 +11,87 @@ namespace ControleEstoque.Tests
         {
         }
 
+        /* 1 - Quando acrescentar 5 itens no produto e quantidade inicial do produto for 20, então deve atualizar quantidade do produto para 25.*/
         [Test]
-        public void Test1()
+        public void QuantidaIgual25_SeEstoqueFor20eAcrescentar5()
         {
-            Assert.Pass();
+            //arrange
+            var produto = new Produto("Carro", "seminovos", 20);
+            //action
+            var produtos = produto.AcrescentarQuantidade(5);
+            //assert
+            Assert.AreEqual(25, produtos);
         }
+        /* 2 - Quando acrescentar 0 itens no produto e quantidade inicial do produto for 20 então não deve atualizar a quantidade de itens do produto.*/
+        [Test]
+        
+        public void QuantidadeNaoMudaSeAcrescentar0()
+        {
+            //arrange
+            var produto = new Produto("Carro", "seminovos", 20);
+            //action e assert
+            QuantidadeMenorOuIgualAZeroException excecao = Assert.Throws<QuantidadeMenorOuIgualAZeroException>(()=>produto.AcrescentarQuantidade(0));           
+           
+        }
+        /* 3 - Quando acrescentar -5 itens no produto e quantidade inicial do produto for 20 então não deve atualizar a quantidade de itens do produto*/
+        [Test]
+        
+        public void QuantidadeNaoMudaSeAcrescentarNumeroNegativo()
+        {
+            //arrange
+            var produto = new Produto("Carro", "seminovos", 20);
+            //action e assert
+            QuantidadeMenorOuIgualAZeroException excecao = Assert.Throws<QuantidadeMenorOuIgualAZeroException>(()=>produto.AcrescentarQuantidade(-5));           
+           
+        }
+
+        [Test]
+        public void QuantidaIgual15_SeEstoqueFor20eDeduzir5()
+        {
+            //arrange
+            var produto = new Produto("Carro", "seminovos", 20);
+            //action
+            var produtos = produto.DeduzirQuantidade(5);
+            //assert
+            Assert.AreEqual(15, produtos);
+        }
+        /*5 - Quando deduzir 0 itens no produto e quantidade inicial do produto for 20 então não deve atualizar a quantidade de itens do produto*/
+        [Test]
+        
+        public void QuantidadeNaoMudaSeDeduzir0()
+        {
+            //arrange
+            var produto = new Produto("Carro", "seminovos", 20);
+            //action e assert
+            QuantidadeMenorOuIgualAZeroException excecao = Assert.Throws<QuantidadeMenorOuIgualAZeroException>(()=>produto.DeduzirQuantidade(0));           
+           
+        }
+
+        /*6 - Quando deduzir -5 itens no produto e quantidade inicial do produto for 20 então não deve atualizar a quantidade de itens do produto*/
+        public void QuantidadeNaoMudaSeDeduzirNumeroNegativo()
+        {
+            //arrange
+            var produto = new Produto("Carro", "seminovos", 20);
+            //action e assert
+            QuantidadeMenorOuIgualAZeroException excecao = Assert.Throws<QuantidadeMenorOuIgualAZeroException>(()=>produto.DeduzirQuantidade(-5));           
+           
+        }
+
+        /*7 - Quando deduzir 6 itens no produto e quantidade inicial do produto for 5 então a quantidade atualizada deve ser 0*/
+        [Test]
+        public void QuantidaIgual0_SeEstoqueFor5eDeduzir6()
+        {
+            //arrange
+            var produto = new Produto("Carro", "seminovos", 5);
+            //action
+            var produtos = produto.DeduzirQuantidade(6);
+            //assert
+            Assert.AreEqual(0, produtos);
+        }
+
+
+
+
+
     }
 }
